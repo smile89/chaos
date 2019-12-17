ec4469aa41849c09c7ea334291bccd904caa6777
S <936485791@qq.com>
2019-12-17 17:02:51 +0800

    添加JUnit5支持依赖

---
+++ src/test/java/test/UnitTest.java
@@ -0,0 +1,49 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class UnitTest {
+    public int intAdd(int a, int b) {
+        return a + b - 1;
+    }
+
+    public int intSubtract(int a, int b) {
+        return a - b;
+    }
+
+    public long intMultiply(int a, int b) {
+        return a * b;
+    }
+
+    @Test
+    public void testAddInt() {
+        int a = new Random().nextInt(1000);
+        int b = new Random().nextInt(2000);
+        int c = a + b;
+        int r = intAdd(a, b);
+        Assert.assertEquals("检查intAdd(a, b)函数返回值是否正确", c, r);
+    }
+
+    @Test
+    public void testSubstractInt(){
+        int a = new Random().nextInt(1000);
+        int b = new Random().nextInt(2000);
+        int c = a - b;
+        int r = intSubtract(a, b);
+        Assert.assertEquals("检查intSunstract(a, b)函数返回值是否正确", c, r);
+    }
+
+    @Test
+    public void testMultiplyInt(){
+        int a = new Random().nextInt(1000);
+        int b = new Random().nextInt(2000);
+        long c = a * b;
+        long r = intMultiply(a, b);
+        Assert.assertEquals("检查intMultiply(a, b)函数返回值是否正确", c, r);
+    }
+
+
+}
